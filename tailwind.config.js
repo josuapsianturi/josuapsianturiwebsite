10d57d23e90eb372368371c9010812fadd715c5b
Josua P. Sianturi <josuapsianturi@gmail.com>
2023-10-25 17:42:24 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'paper': '#fffdf7',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
